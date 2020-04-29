21a4459fda73262cdaf0826982c44958a13fee23
Tein van der Lugt <teinvdlugt@gmail.com>
2020-04-29 16:16:24 +0200

    Add growth and mitosis

--- Model/Simulation/Model.py
+++ Model/Simulation/Model.py
@@ -1,7 +1,8 @@
 from cc3d import CompuCellSetup
-
-from .ModelSteppables import InitialiserSteppable
+from .ModelSteppables import *  # You might need to remove the dot
 
 CompuCellSetup.register_steppable(steppable=InitialiserSteppable())
+CompuCellSetup.register_steppable(steppable=VolumeSteppable())
+CompuCellSetup.register_steppable(steppable=MitosisSteppable())
 
 CompuCellSetup.run()
--- Model/Simulation/ModelSteppables.py
+++ Model/Simulation/ModelSteppables.py
@@ -1,8 +1,17 @@
 from cc3d.core.PySteppables import *
 
-# Model parameters
+# Collagen parameters
 collagen_length = 10
 collagen_thickness = 1
+
+# Volume, growth and mitosis parameters
+tumor_initial_volume = 64.0  # must agree with PIF file
+tumor_growth_rate = 0.05  # per MCS -- keep this a float
+tumor_lambda_volume = 10.0  # from Scianna et al.
+mitosis_threshold = 100  # cell divides if volume > mitosis_threshold
+volume_steppable_frequency = 10  # Maybe change this frequency. I have set it to 10 to reduce computation
+
+collagen_lambda_volume = 11.0  # from Scianna et al.
 
 
 class InitialiserSteppable(SteppableBasePy):
@@ -11,13 +20,47 @@ class InitialiserSteppable(SteppableBasePy):
         SteppableBasePy.__init__(self, frequency)
 
     def start(self):
+        # Nothing here.
+        return
+
+
+class VolumeSteppable(SteppableBasePy):
+    def __init__(self, frequency=volume_steppable_frequency):
+        SteppableBasePy.__init__(self, frequency)
+
+    def start(self):
+        # Initialise cell volumes
         for cell in self.cellList:
-            if cell.type == 2: # Tumor
-                cell.targetVolume = 64  # 8x8 pixels
-                cell.lambdaVolume = 10  # from Scianna et al.
-            if cell.type == 1: # Collagen
+            if cell.type == self.TUMOR:
+                cell.targetVolume = tumor_initial_volume
+                cell.lambdaVolume = tumor_lambda_volume
+            if cell.type == self.COLLAGEN:
                 cell.targetVolume = collagen_length * collagen_thickness
-                cell.lambdaVolume = 11
+                cell.lambdaVolume = collagen_lambda_volume
+
+    def step(self, mcs):
+        for cell in self.cell_list:
+            if cell.type == self.TUMOR:
+                cell.targetVolume += tumor_growth_rate * volume_steppable_frequency
 
 
-# Maybe add growth, mitosis steppables
+class MitosisSteppable(MitosisSteppableBase):
+    # Docs: https://pythonscriptingmanual.readthedocs.io/en/latest/mitosis.html
+    def __init__(self, frequency=10):  # Maybe change this frequency. I have set it to 10 to reduce computation
+        MitosisSteppableBase.__init__(self, frequency)
+        # Randomise where the 'parent' will end up and where the 'child' will end up (see docs):
+        self.set_parent_child_position_flag(0)
+
+    def step(self, mcs):
+        cells_to_divide = []
+        for cell in self.cell_list:
+            if cell.type == self.TUMOR and cell.volume > mitosis_threshold:
+                cells_to_divide.append(cell)
+        for cell in cells_to_divide:
+            self.divide_cell_random_orientation(cell)  # for other orientations, see docs
+
+    def update_attributes(self):
+        # Called after mitosis has occurred.
+        # Set new target volumes:
+        self.parent_cell.targetVolume /= 2.0
+        self.clone_parent_2_child()  # Copy parent cell parameters to daughter cell
