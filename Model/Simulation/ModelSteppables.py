75cc9c756772b888dbd6258402cfc02a31f8e6ef
Tein van der Lugt <teinvdlugt@gmail.com>
2020-05-20 20:39:48 +0200

    Finish merge

--- Model/Simulation/ModelSteppables.py
+++ Model/Simulation/ModelSteppables.py
@@ -1,20 +1,20 @@
 from cc3d.core.PySteppables import *
 import math
 
+# 3D toggle. To toggle 3D, set to True and change some lines in Model.xml
 _3d = False
 
 # Volume, surface, growth and mitosis parameters
 tumor_lambda_volume = 10.0  # from Scianna et al.
-tumor_initial_surface = 8 * sqrt(tumor_initial_volume / math.pi)  # See also volume_to_surface function
 tumor_lambda_surface = 2.0  # TODO what does Scianna say?
-
-
 tumor_growth_rate = 0.01  # per MCS -- be sure to keep this a float
+collagen_lambda_volume = 11.0  # from Scianna et al.
+mmp_offset = 50  # The amount of mmp constantly secreted
 
+# Steppable frequencies
+volume_steppable_frequency = 20  # Maybe change this frequency. The higher the cheaper computation
+mmpdegradation_steppable_frequency = 10  # mmpdegradation turns out te be extremely expensive
 
-mitosis_threshold = 2 * tumor_initial_volume  # cell divides if volume > mitosis_threshold
-volume_steppable_frequency = 20  # Maybe change this frequency. I have set it to 10 to reduce computation
-mmpdegradation_steppable_frequency = 10 # mmpdegradation turns out te be extremely expensive
 
 # To increase speed, consider changing every call to this function to the appropriate function (2d or 3d)
 def volume_to_surface(volume):
@@ -23,7 +23,6 @@ def volume_to_surface(volume):
     else:
         return volume_to_surface2d(volume)
 
-mmp_offset = 50 # The amount of mmp constantly secreted
 
 _8oversqrtpi = 8 / sqrt(math.pi)
 def volume_to_surface2d(volume):
@@ -48,7 +47,7 @@ class VolumeSurfaceSteppable(SteppableBasePy):
         for cell in self.cellList:
             if cell.type == self.TUMOR:
                 cell.targetVolume = cell.volume
-                cell.targetSurface = volume_to_surface(cell.targetVolume)
+                cell.targetSurface = volume_to_surface(cell.volume)
                 cell.lambdaVolume = tumor_lambda_volume
                 cell.lambdaSurface = tumor_lambda_surface
 
@@ -107,16 +106,18 @@ class MMPSecretionSteppable(SecretionBasePy):
         for cell in self.cell_list:
             if cell.type == self.TUMOR:
                 # Secretion rate depends on cell confinement
-                delta_volume = cell.targetVolume - cell.volume
-                #delta_surface = cell.targetSurface - cell.surface
-                delta_surface = 0 # I think we decided on leaving out the surface term in the secretion of the mmp field
-                delta_volume = 0 if delta_volume < 0 else delta_volume
-                delta_surface = 0 if delta_surface < 0 else delta_surface
-                
-                confinement_energy = tumor_lambda_volume * delta_volume ** 2 \
-                                     + tumor_lambda_surface * delta_surface ** 2 # Why should this be quadratic?
-                
-                secr_rate = (confinement_energy+mmp_offset) * 5e-5 # How should secretion rate depend on contact area?
+                # Secretion rate should be increasing function of "cell.targetVolume - cell.volume" but
+                # should be 0 when  cell.targetVolume - cell.volume < 0.
+
+                # The following expression is quadratic because it was inspired by the volume-energy term
+                # The surface energy contribution is commented out.
+                confinement_energy = tumor_lambda_volume * max(cell.targetVolume - cell.volume, 0) ** 2
+                # + tumor_lambda_surface * max(cell.targetSurface - cell.surface, 0) ** 2
+
+                # Add offset and scale appropriately
+                secr_rate = (confinement_energy + mmp_offset) * 5e-5
+
+                # MMP is secreted at secr_rate at all pixels that are neighbour of a collagen pixel.
                 secretor.secreteOutsideCellAtBoundaryOnContactWith(cell, secr_rate, [self.COLLAGEN])
 
 
@@ -125,19 +126,18 @@ class MMPDegradationSteppable(SteppableBasePy):
         SteppableBasePy.__init__(self, frequency)
 
     def start(self):
-        # Find medium cell object (to replace collagen pixels with)  # TODO only once?
+        # Find medium cell object (to replace collagen pixels with)
         for cell in self.cell_list:
             if cell.type == self.MEDIUM:
                 self.medium_cell = cell
                 break
 
     def step(self, mcs):
-        
         mmp = CompuCell.getConcentrationField(self.simulator, "MMP")
 
         for cell in self.cell_list_by_type(self.COLLAGEN):
             for pixel in self.get_copy_of_cell_pixels(cell):
-                if mmp.get(pixel) >= 1: # Maybe add some stochastic factor into this?
+                if mmp.get(pixel) >= 1: # TODO Maybe add some stochastic factor into this?
                     # Replace Collagen by Medium:
                     self.cell_field[pixel.x, pixel.y, pixel.z] = self.medium_cell
                     # Remove MMP:
